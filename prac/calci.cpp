92c430de86f4ae747dd04c1ef9fd5e14dac34980
Aman <amangandhiyolo@gmail.com>
2022-09-03 18:21:20 +0530

    practise added

---
+++ prac/calci.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  char ch;
+  float num1, num2;
+  cout << "Enter an operator (+, -, *, /):  ";
+  cin >> ch;
+  cout << "Enter two operands: ";
+  cin >> num1 >> num2;
+  switch(ch)
+  {
+    case '+':
+      cout << num1+num2;
+      break;
+    case '-':
+      cout << num1-num2;
+      break;
+    case '*':
+      cout << num1*num2;
+      break;
+    case '/':
+      cout << num1/num2;
+      break;
+    default:
+      // If the operator is other than +, -, * or /, error message is shown
+      cout << "Error! operator is not correct";
+      break;
+  }
+  return 0;
+}
